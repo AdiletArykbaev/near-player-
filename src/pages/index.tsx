ec020300e881ee8fceda5be24ac23677fe454ea4
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-09-03 19:16:05 +0600

    sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import Market from "./Market";
+import Header from "../shared/ui/Header";
 
 const Routing = () => {
     return (
         <>
+            <Header/>
             <Routes>
                 <Route path={"/"} element={<Market/>}/>
             </Routes>
