1af584428607829b45d4144171fc320de65e944a
Abdurasulov Maksat <maxabdurasulov@gmail.com>
2022-09-03 18:09:03 +0600

    fix bugs

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
-import { withProviders } from "./providers";
+import React from "react";
+import {withProviders} from "./providers";
 
 function App() {
-   return (
-      <div className="App">
-      </div>
-   );
+    return (
+        <div className="App">
+            GOOD JOB!
+        </div>
+    );
 }
 
 export default withProviders(App);
