fac1624b6c91974b240ba5c4fd698e5072cc17d9
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-09-05 00:49:33 +0600

    player page

--- src/pages/NftDetails/NftDetails.tsx
+++ src/pages/NftDetails/NftDetails.tsx
@@ -1,10 +1,18 @@
 import React, {FC, useEffect, useState, useRef} from 'react';
 import AudioWave from "../../features/ui/AudioWave/MusicPlayer";
 import Header from "../../shared/ui/Header";
-import {Link} from "react-router-dom";
 import "./NftDetails.scss";
 import AudioPreview from "./AudioPreview/AudioPreview";
 import AudioPlayer from "./AudioPlayer/AudioPlayer";
+import ButtonBack from "../../features/ui/ButtonBack";
+import {Link} from "react-router-dom";
+import NftDes from "./NftDescription/NftDes";
+import SwitchCaser from "./Switcher/SwitchCaser";
+import Counter from "./Counter/Counter";
+import Owner from "./Owner/Owner";
+import Offers from "./Offers/Offers";
+import RecentCard from "../../features/ui/RecentCard";
+import CardCarousel from "../../features/ui/CardCarousel";
 
 interface Props {
     url: string
@@ -13,27 +21,39 @@ interface Props {
 const NftDetails: FC<Props> = ({url}) => {
 
     const [playing, setPlaying] = useState(false);
+    const [switcher, setSwitcher] = useState(1);
+    const [count, setCounter] = useState(0);
     const audioRef: any = useRef(null);
-
-    useEffect(()=>{
-        console.log(audioRef.current.currentTime)
-    }, [audioRef.current]);
 
     return (
         <div className="NftDetails">
             <Header type={false}/>
-
+            <Link to={"/explore"}>
+                <ButtonBack/>
+            </Link>
             <div className="NftInfo">
                 <AudioPreview setPlaying={setPlaying} playing={playing}/>
                 <div className="Right">
-                    <AudioWave  setPlay={setPlaying} playing={playing}/>
+                    <NftDes/>
+                    <AudioWave ref={audioRef} setPlay={setPlaying} playing={playing}/>
+                    <SwitchCaser switcher={switcher} setSwitcher={setSwitcher}/>
+                    <Counter count={count} setCount={setCounter}/>
+                    <Owner/>
+                    <Offers/>
+                    <div className="OtherArtists">
+                        <CardCarousel type={3} text={"Other artist’s tracks "}
+                                      data={Array.from(Array(8).keys())} slidesCount={3} rowCount={1}/>
+                    </div>
+
+
                 </div>
             </div>
-
-            <AudioPlayer/>
-
-            <audio ref={audioRef} id="track" src={url}/>
-
+            {
+                playing
+                    ? <AudioPlayer classProps={"up"}/>
+                    : <AudioPlayer classProps={"down"}/>
+            }
+            <audio id="track" src={url}/>
         </div>
     );
 };
---
+++ src/pages/NftDetails/Offers/Offers.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Offers.scss"
+
+const Offers = () => {
+    return (
+        <div className="Offers">
+                <button className="Button">
+                    No offers received
+                </button>
+        </div>
+    );
+};
+
+export default Offers;
